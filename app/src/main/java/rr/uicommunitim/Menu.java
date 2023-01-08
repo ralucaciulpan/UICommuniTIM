f98d77ce43798cd29d4a7e178b5c6bcd4ee32b8b
pozaradugabriel <poza.radu@gmail.com>
2023-01-08 05:17:13 +0200

    fixed homepage button color and hid the title bar

--- app/src/main/java/rr/uicommunitim/Menu.java
+++ app/src/main/java/rr/uicommunitim/Menu.java
@@ -9,5 +9,6 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        getSupportActionBar().hide();
     }
 }

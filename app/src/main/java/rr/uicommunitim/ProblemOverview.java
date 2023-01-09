ea6e2e11b355cb0b4040b136212a351c6f94cd72
Raluca Ciulpan <raluuu26@gmail.com>
2023-01-09 22:11:16 +0200

    created problem overview page xml

---
+++ app/src/main/java/rr/uicommunitim/ProblemOverview.java
@@ -0,0 +1,14 @@
+package rr.uicommunitim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProblemOverview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_problem_overview);
+    }
+}
